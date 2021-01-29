d947984de43b7a1dc73b0ed19d8933d00eca237e
rafaelmeirelles8 <56368908+rafaelmeirelles8@users.noreply.github.com>
2021-01-29 09:37:07 -0300

    Burger with ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,14 +5,22 @@ import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
 
-
+state = {
+    ingredients: {
+        salad:1,
+        bacon:1,
+        cheese:2,
+        meat:2
+    },
+    price:0
+}
 
 
     render()
     {
         return(
             <Auxiliary>
-                <Burger></Burger>
+                <Burger ingredients={this.state.ingredients}></Burger>
                 <div>Build Controls</div>
             </Auxiliary>
         )
