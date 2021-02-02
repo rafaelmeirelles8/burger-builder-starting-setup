8064daaef3cbaab346dd2caa39e041778eb98b78
rafaelmeirelles8 <56368908+rafaelmeirelles8@users.noreply.github.com>
2021-02-02 14:28:53 -0300

    Logo Toolbar responsive.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Logo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
-const logo = () => (
-    <div className={classes.Logo}>
+const logo = (props) => (
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={Logo} alt="MyBurger" />
     </div>
 );
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
     return (
-        <div>
-
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>
         </div>
     );   
 };
